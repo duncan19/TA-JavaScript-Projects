cabe1357840007acb44550b78c6c52d263798dc9
duncan19 <duncanrobbins19@gmail.com>
2021-08-23 11:34:39 -0700

    Add JS

--- Basic JavaScript Projects/scripts.js
+++ Basic JavaScript Projects/scripts.js
@@ -1,6 +1,14 @@
+//
+// var myVar="this is my variable"
+// var x=" this is another variable"
+// var game1="Overwatch", game2="League of Legends", game3="Dead by Daylight"
+//
+// document.write(game1 + " is worse than "+ game3)
+// document.write(4+4)
+// window.alert(myVar)
 
-var myVar="this is my variable"
-var x=" this is another variable"
-document.write(myVar + x)
-
-window.alert(myVar)
+function My_First_Func() {
+  var str = "This text is blue" //Creates string//
+  var result = str.fontcolor("blue") //Sets the string to be blue//
+  document.getElementByID("Blue_Text").innerHTML = result; //Creates it in html//
+}
