777793358cd23df0b8c2f7f587c2ed3cb07c41c1
duncan19 <duncanrobbins19@gmail.com>
2021-08-24 10:49:31 -0700

    Add concatenation

--- Basic JavaScript Projects/scripts.js
+++ Basic JavaScript Projects/scripts.js
@@ -9,6 +9,7 @@
 
 function My_First_Func() {
   var str = "This text is blue" //Creates string//
+  str  += " and here is some more text" //updates the str var to include more text//
   var result = str.fontcolor("blue") //Sets the string to be blue//
   document.getElementById("Blue_text").innerHTML = result; //Creates it in html//
 }
