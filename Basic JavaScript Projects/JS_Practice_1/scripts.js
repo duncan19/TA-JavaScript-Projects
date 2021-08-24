f75745d251f653431e65e30ef10393c60127d43f
duncan19 <53705958+duncan19@users.noreply.github.com>
2021-08-24 11:22:15 -0700

    Update scripts.js

--- Basic JavaScript Projects/JS_Practice_1/scripts.js
+++ Basic JavaScript Projects/JS_Practice_1/scripts.js
@@ -1,11 +1,4 @@
-//
-// var myVar="this is my variable"
-// var x=" this is another variable"
-// var game1="Overwatch", game2="League of Legends", game3="Dead by Daylight"
-//
-// document.write(game1 + " is worse than "+ game3)
-// document.write(4+4)
-// window.alert(myVar)
+
 
 function My_First_Func() {
   var str = "This text is blue" //Creates string//
