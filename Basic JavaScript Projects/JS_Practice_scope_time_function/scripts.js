84fb15067b66d8e8e003cd5a357a9a9e37e5cd73
duncan19 <duncanrobbins19@gmail.com>
2021-08-25 11:53:50 -0700

    Update time func practice

--- Basic JavaScript Projects/JS_Practice_scope_time_function/scripts.js
+++ Basic JavaScript Projects/JS_Practice_scope_time_function/scripts.js
@@ -1,33 +1,51 @@
-
-
-function Vehicle(Make, Model, Year, Color) {  //make constructor //
-  this.Vehicle_Make = Make;  //assign properties//
-  this.Vehicle_Model = Model; //assign properties//
-  this.Vehicle_Year = Year; //assign properties//
-  this.Vehicle_Color = Color; //assign properties//
+var z = 10
+function My_Func(){
+  var x = 10
+  x = z + x + 4
+  y = x +2
+  console.log(x)
 }
-var Bob = new Vehicle("Dodge", "Viper", 2020, "Red"); //Create new variable using constructor//
-var Joe = new Vehicle("Jeep", "Trail Hawk", 2019, "White"); //Create new variable using constructor//
-var Jack = new Vehicle("Ford", "Pinto", 1971, "Blue"); //Create new variable using constructor//
-
-function my_Func() {
-  document.getElementById("Keywords_and_Constructors").innerHTML = //Sets the paragrah to display info of bob's vehicle//
-  "Bob drives a " + Bob.Vehicle_Color + "-colored " + Bob.Vehicle_Model + " manufactured in " + Bob.Vehicle_Year;
+function My_Func2(){
+  console.log(z)
+  console.log(x)
+  console.log(y)
 }
-
-function count_Func() {
-  document.getElementById("Counting").innerHTML = Count() ; //run the dunction Count//
-  function Count() {
-    var Starting_point = 9; //set starting point to 9//
-    function Plus_one() {Starting_point += 1;} //add one to starting point//
-    Plus_one(); //run function//
-    return Starting_point; //update var//
+function My_Func3(){
+if (4 < 2) {
+  console.log("true");
+}
+else {
+  console.log("False");
+}
+}
+function My_Func4(){
+  if (new Date().getHours() < 14) {
+    console.log("how are you today");
   }
 }
 
-function Vote_Func() {
-  var Age, Can_vote; //create two variables//
-  Age = document.getElementById("Age").value //set age to the input from user//
-  Can_vote = (Age < 52) ? "You are too young":"You are old enough"; //test to see if age is true or false//
-  document.getElementById("Vote").innerHTML = Can_vote + " to vote."; //update html//
+function Age_Func() {
+  Age= document.getElementById("Age").value;
+  if (Age >= 18 ) {
+    Vote = "You are old enough to vote!";
+  }
+  else {
+    Vote = "You are not old enough to vote";
+  }
+  document.getElementById("How_old_are_you").innerHTML = Vote;
+}
+
+function Time_Func() {
+  var Time = new Date().getHours();
+  var Reply = "";
+  if (Time < 12 == Time > 0) {
+    Reply = "It is morning time!"
+  }
+  else if (Time >= 12 == Time < 0) {
+    Reply = "It is afternoon!"
+  }
+  else {
+    Reply = "It is night time"
+  }
+  document.getElementById("Time_of_day").innerHTML = Reply;
 }
