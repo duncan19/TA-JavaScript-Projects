3f1931c7add29f11fcaad9f30946d6ef6f8f2b29
duncan19 <duncanrobbins19@gmail.com>
2021-08-26 09:22:42 -0700

    update string methods

--- Basic JavaScript Projects/JS_Practice_string_methods/scripts.js
+++ Basic JavaScript Projects/JS_Practice_string_methods/scripts.js
@@ -1,1 +1,9 @@
-
+function full_Sentence() {
+  var part_1= "I have "
+  var part_2= "made this "
+  var part_3= "into a "
+  var part_4= "complete  "
+  var part_5= "sentence."
+  var whole_sentence = part_1.concat(part_2, part_3, part_4, part_5)
+  document.getElementById("Concatenate").innerHTML = whole_sentence
+}
